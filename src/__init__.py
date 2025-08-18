b0d8b63bb6604f5f2f3d4526074758bf9064491e
khushali9 <khushali.desai9@gmail.com>
2025-08-18 07:22:43 -0700

    created project structure with log and exception

---
+++ src/__init__.py
@@ -0,0 +1,2 @@
+# to identify this folder as a package
+# and to allow imports from this package
---
+++ src/exception.py
@@ -0,0 +1,32 @@
+import sys
+def error_message_detail(error,error_detail:sys):
+    """
+    This function returns a detailed error message.
+    :param error: The exception object.
+    :param error_detail: The sys module.
+    :return: A string containing the error message.
+    """
+
+    _,_,exc_tb = error_detail.exc_info()#exc_tb is the traceback object
+    # Extracting the file name and line number from the traceback object
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_message = f"Error occurred in python script: [{file_name}] line number [{exc_tb.tb_lineno}] error message [{str(error)}]"
+    return error_message
+
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail:sys):
+        """
+        Custom exception class that inherits from the built-in Exception class.
+        :param error_message: The error message to be displayed.
+        :param error_detail: The sys module for detailed error information.
+        """
+        super().__init__(error_message) #cause inheriting from Exception
+        self.error_message = error_message_detail(error_message, error_detail)
+    
+    def __str__(self):
+        """
+        Returns the string representation of the error message.
+        :return: The error message.
+        """
+        return self.error_message #when we print the object, it will call this method and print the error message
+    
